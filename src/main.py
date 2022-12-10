7b5e4363d457dd0b7f176be710f38fa41a96349f
lycheejam <lycheejamwow@gmail.com>
2022-12-11 02:04:58 +0900

    :sparkles: やっていき

--- src/main.py
+++ src/main.py
@@ -31,6 +31,43 @@ class ManipulationSpreadsheet:
         except Exception:
             print(traceback.format_exc())
 
+    def create_spreadsheet(self, authorized_credential, spreadsheet_name):
+        try:
+            return authorized_credential.create(spreadsheet_name)
+        except Exception:
+            print(traceback.format_exc())
+
+    def get_spreadsheet(self, authorized_credential, spreadsheet_id):
+        try:
+            return authorized_credential.open_by_key(spreadsheet_id)
+        except Exception:
+            print(traceback.format_exc())
+
+    def get_sheet(self, spreadsheet):
+        try:
+            return spreadsheet.sheet1()
+        except Exception:
+            print(traceback.format_exc())
+
+    def share_spreadsheet(self, spreadsheet):
+        # NOTE: replace mail address
+        response = spreadsheet.share(
+            "example@example.com", perm_type="user", role="writer"
+        )
+
+        return response
+
+    def value_update_spreadsheet(self, spreadsheet, formated_results):
+        response = spreadsheet.values_update(
+            "Sheet1!A1",
+            params={
+                "valueInputOption": "RAW",
+            },
+            body={"values": formated_results},
+        )
+
+        return response
+
 
 if __name__ == "__main__":
     get_data_to_spreadsheet = ManipulationSpreadsheet()
